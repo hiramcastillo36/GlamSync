a4da07e4db83f0a925fd1b82ff547d46b3f2ce4d
Hiram Castillo <hiramcastillo36@gmail.com>
2025-05-11 01:01:49 -0600

    ✨ feat: add registration route to application routing configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../app/pages/login/login.component';
 import { HomeComponent } from '../app/pages/home/home.component';
 import { SalonDetailComponent } from '../app/pages/salon-detail/salon-detail.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'salon/:id', component: SalonDetailComponent }
+  { path: 'salon/:id', component: SalonDetailComponent },
+  { path: 'register', component: RegisterComponent }
 ];
