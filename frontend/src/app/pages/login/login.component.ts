3ada005088f0e43d18ad040831e0d30311dd4122
Hiram Castillo <hiramcastillo36@gmail.com>
2025-05-11 01:01:42 -0600

    ✨ feat: enhance login component with authentication service integration and form validation

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
@@ -18,18 +19,31 @@ export class LoginComponent {
 
   constructor(
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
   ) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
-      username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
+
+
   onSubmit(): void {
     if (this.loginForm.valid) {
-      this.router.navigate(['/home']);
+      this.authService.login(this.loginForm.value).subscribe(
+        {
+          next: (res) => {
+            this.authService.saveToken(res.token);
+            this.authService.saveUser(res.user);
+            this.router.navigate(['/home']);
+          },
+          error: (err) => {
+            console.log(err);
+          }
+        }
+      );
     } else {
       Object.keys(this.loginForm.controls).forEach(key => {
         this.loginForm.get(key)?.markAsTouched();
