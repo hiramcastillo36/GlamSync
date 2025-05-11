966665d32194dc49b75808d685f671e64604e858
Hiram Castillo <hiramcastillo36@gmail.com>
2025-05-11 14:04:51 -0600

    ✨ feat: add route to fetch all salons in the salon API

--- backend/routes/salon.js
+++ backend/routes/salon.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { getSalonById, createSalon, updateSalon, deleteSalon } = require("../controllers/salones");
+const { getSalonById, createSalon, updateSalon, deleteSalon, getSalones } = require("../controllers/salones");
 const router = Router();
 
 router.get("/:id", getSalonById);
 router.post("/", createSalon);
+router.get("/", getSalones);
 router.put("/:id", updateSalon);
 router.delete("/:id", deleteSalon);
 
