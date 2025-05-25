53d6ba2c9ec979e4d9cfc75ddb25f82f47276610
Hiram Castillo <hiramcastillo36@gmail.com>
2025-05-25 13:15:57 -0600

    ✨ feat: update AppointmentData interface to use correct Service and Package types

--- frontend/src/app/interfaces/appointment.ts
+++ frontend/src/app/interfaces/appointment.ts
@@ -1,12 +1,12 @@
-import { SalonDetail } from './salon.interface';
+import { SalonDetail, Service } from './salon.interface';
 import { ServiceItem } from './service';
-import { PackageItem } from './package';
+import { Package } from './package.interface';
 import { Professional } from './professional';
 
 export interface AppointmentData {
   salon: SalonDetail;
-  servicio: ServiceItem | null;
-  paquete: PackageItem | null;
+  servicio: Service | null;
+  paquete: Package | null;
   fecha: Date;
   horario: string;
   persona: Professional | null;
