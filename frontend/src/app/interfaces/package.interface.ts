c2a395c314ce5d49b714ebb6a7d99c05d7b63e4e
Hiram Castillo <hiramcastillo36@gmail.com>
2025-05-25 13:14:44 -0600

    ✨ feat: create Package and PackageResponse interfaces for improved package data structure

---
+++ frontend/src/app/interfaces/package.interface.ts
@@ -0,0 +1,13 @@
+import { ID } from './types';
+
+export interface Package {
+  id: ID;
+  name: string;
+  description: string;
+  price: number;
+  services: ID[];
+}
+
+export interface PackageResponse {
+  data: Package[];
+}
